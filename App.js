924ebe326df0651a4d4b7393cb426889483afc0e
Alikhan Gubayev <AlikhanGubayev@MacBook-Air-Air.local>
2022-07-11 19:02:15 +0600

    arlenu-backend

--- App.js
+++ App.js
@@ -7,7 +7,8 @@ const itemController = require("./controllers/item");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb" }));
 
 app.use("/user", userController);
 app.use("/item", itemController);
--- models/Item.js
+++ models/Item.js
@@ -5,7 +5,8 @@ const { Schema } = mongoose;
 const itemSchema = new Schema({
   title: { type: String, default: null },
   description: { type: String, default: null },
-  image: { type: String, default: null, required: true },
+  image: { type: String, default: null },
+  likes: { type: Number, default: 0 },
 }, { timestamps: true });
 
 const Item = mongoose.model("Item", itemSchema);
